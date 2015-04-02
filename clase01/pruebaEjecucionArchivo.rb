ace45d15ec3a0a3fbe10cac7c4d196759bb2860f
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2015-04-02 01:24:05 -0300

    Primera clase con ruby

---
+++ clase01/pruebaEjecucionArchivo.rb
@@ -0,0 +1,16 @@
+puts "Bienvenidos al maravilloso mundo de ruby"
+
+# Una simple suma
+puts 2 + 3
+
+# Multiplicacion
+puts 3 * 6
+
+# ELevar al cuadrado
+puts 3 ** 2
+
+# Inprime una cadena al revez 'oigrol'
+puts "lorgio".reverse
+
+# Imprime todo en mayuscula
+puts "lorgio".upcase
