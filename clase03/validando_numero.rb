a8d7b6c61d9f16f8e3d2cb51c95d0f4ddbe13b22
Lorgio Trinidad <lorgiotrinidad@gmail.com>
2015-04-05 00:04:19 -0300

    validando numero

---
+++ clase03/validando_numero.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+numero = 3
+
+if numero > 5
+  puts "#{numero} es mayor que 5"
+elsif numero == 3
+  puts "#{numero} es igual a 3"
+else 
+  puts "#{numero} es menor que 5 pero no es igual a 3"
+end
+
+if numero > 3
+  puts "#{numero} es mayor que 3"
+end
+
+isVerdadero = false
+
+if (!isVerdadero) and (numero >= 3)
+  puts "la variable es false y el numero es mayor o igual a 3"
+end
+
